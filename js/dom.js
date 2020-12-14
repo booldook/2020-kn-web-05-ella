9a4c24bf1a745748d44303292b922a0559520da3
booldook <booldook@gmail.com>
2020-12-14 17:02:41 +0900

    완성

--- js/dom.js
+++ js/dom.js
@@ -12,3 +12,20 @@
  * 10. RegExp - 정규표현식
  * 11. Object - 객체
  */
+
+var colors = ['red', 'green', 'orange', 'blue', 'pink'];
+
+function onCreateBox() {
+	var cnt = Number($("input[name='cnt']").val());
+	for(var i=0, color; i<cnt; i++) {
+		color = Math.ceil(Math.random() * 5) - 1;
+		$(".wrapper").append('<div class="box '+colors[color]+'"></div>');
+	}
+}
+
+function onResetBox() {
+	$(".wrapper").empty();
+}
+
+$("#btCreate").click(onCreateBox);
+$("#btReset").click(onResetBox);
