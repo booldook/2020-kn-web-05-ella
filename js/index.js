cb3026fe07b7c154f217008c963943cd00c08424
booldook <booldook@gmail.com>
2020-12-14 11:59:38 +0900

    ...

--- js/index.js
+++ js/index.js
@@ -7,9 +7,13 @@
 /********* 이벤트선언 **********/
 $('.top-wrapper .icon-down').click(onLangChg); // 언어선택
 $('.top-wrapper .bt-down').click(onLangSel); // 언어선택
+$.get('../json/new-products.json', onNewProducts); // new releases 상품 가져오기
 
 
 /********* 이벤트콜백 **********/
+function onNewProducts(r) {
+	console.log(r);
+}
 function onLangChg() {
 	$(".trans-wrapper").stop().slideToggle(200);
 	$(".trans-wrapper .lang-sel").stop().slideUp(200);
