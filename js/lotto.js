c721e7fd6a27f8ff5e2fcc448266a61f6110bcf4
booldook <booldook@gmail.com>
2020-12-15 11:11:12 +0900

    ...

--- js/lotto.js
+++ js/lotto.js
@@ -11,6 +11,13 @@
  * 
  *? 다 만들면 booldook@gmail.com으로 폴더 전체를 압축하여 보낸다.
  * 보내는 제목 : [홍길동] 로또 숙제
+ * ===============================================================
+ * Array Method
+ * push() // 맨 뒤에 요소 추가
+ * pop() // 맨 뒤에서 요소 추출
+ * shift() // 맨 앞에 요소 추가
+ * unshift() // 맨 앞에서 요소 추출
+ * sort() // 문자열 오름차순으로 정렬 (콜백함수로 숫자의 오름차순, 내림차순을 할 수 있다.)
  */
 
 var lotto = [];				// 로또 번호를 담을 배열
@@ -33,9 +40,45 @@ function genLottoHtml(arr) {
 	return html;
 }
 
+/* function onLucky() {
+	legacyLotto = lotto;
+	lotto = [];
+	var number;			// 추출한 번호를 담을 변수
+	var color;			// 생성될 공의 class를 담을 변수
+	
+	// 1. 6개의 랜덤한 번호를 lotto에 넣는다.
+	while(lotto.length < 6) {
+		number = Math.floor(Math.random() * 45) + 1;
+		if(lotto.indexOf(number) == -1) lotto.push(number);
+	}
+
+	// 2. lotto를 오름차순으로 정렬한다.
+	// return a - b; // 오름차순
+	// return b - a; // 내림차순
+	lotto.sort(function(a, b) { return a - b; });
+	
+	// 3. 기존의 번호를 지운고, 공을 생성해서 화면에 그린다.
+	$(".result-wrap").empty().append(genLottoHtml(lotto));
+	
+	// 4. 기존 번호(legacyLotto)로 아래에 그린다.
+	if(legacyLotto.length > 0) {
+		html  = '<div class="history">';
+		html += genLottoHtml(legacyLotto);
+		html += '</div>';
+		$(".history-wrapper").prepend(html);
+	}
+} */
+
 function onLucky() {
 	legacyLotto = lotto;
 	lotto = [];
+	
+	var defaultLotto = [];
+	for(var i=1; i<=45; i++) defaultLotto.push(i);
+	while(lotto.length < 6) {
+
+	}
+
 	var number;			// 추출한 번호를 담을 변수
 	var color;			// 생성될 공의 class를 담을 변수
 	
