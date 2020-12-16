7a5a26475ffe9ff3437bfb3276eca7069a36093f
booldook <booldook@gmail.com>
2020-12-16 10:27:20 +0900

    ...

---
+++ js/booldook-slide.js
@@ -0,0 +1,36 @@
+/**
+ *! 전역변수
+ */
+var $container = $('.booldook-slide');
+var $stage = $('.booldook-slide').find('.slide-stage');
+var $wrapper = $('.booldook-slide').find('.slide-wrapper');
+var $slide = $('.booldook-slide').find('.slide');
+
+/**
+ *! 사용자 함수
+ */
+function init() {
+
+}
+
+/**
+ *! 이벤트 콜백
+ */
+
+
+
+
+/**
+ *! 이벤트 등록
+ */
+
+
+
+
+
+
+
+
+
+
+init();
