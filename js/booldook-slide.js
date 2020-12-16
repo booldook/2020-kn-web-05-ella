41cdf724388544891777c1c795e3b784938db3ae
booldook <booldook@gmail.com>
2020-12-16 12:25:27 +0900

    생성과 반응형처리

--- js/booldook-slide.js
+++ js/booldook-slide.js
@@ -1,14 +1,22 @@
 /**
+ *! 프로그램 세팅값
+ */
+var standard = '.booldook-slide';
+var stageDefault = 5;	// 스테이지에 보여질 개수 기준값
+
+
+/**
  *! 전역변수
  */
-var $container = $('.booldook-slide');
-var $stage = $('.booldook-slide').find('.slide-stage');
-var $wrapper = $('.booldook-slide').find('.slide-wrapper');
-var $slide = $('.booldook-slide').find('.slide');
+var $container = $(standard);
+var $stage = $(standard).find('.slide-stage');
+var $wrapper = $(standard).find('.slide-wrapper');
+var $slide = $(standard).find('.slide');
 var slideCnt = $slide.length;	// 슬라이드의 총 개수 length
 var slideLast = slideCnt - 1; // 슬라이드의 마지막 index 
-var stageCnt = 4;	// 스테이지에 보여질 개수
-var now = 3;
+var slideWid;	// 슬라이드의 width
+var slideCnt;	// 스테이지에 보여질 개수의 반응형에 따른 변하는 값
+var now = 0;
 
 
 /**
@@ -16,27 +24,37 @@ var now = 3;
  */
 function init() {
 	$wrapper.empty();
-	$slide.eq(now).clone().appendTo($wrapper);
+	$slide.eq(now).clone().appendTo($wrapper).css("width", slideWid+"%");
 	for(var i=0, my, prev=now; i<stageCnt; i++) {
 		prev = my = (prev == 0) ? slideLast : prev - 1;
-		$slide.eq(my).clone().prependTo($wrapper);
+		$slide.eq(my).clone().prependTo($wrapper).css("width", slideWid+"%");
 	}
 	for(var i=0, my, prev=now; i<stageCnt*2-1; i++) {
 		prev = my = (prev == slideLast) ? 0 : prev + 1;
-		$slide.eq(my).clone().appendTo($wrapper);
+		$slide.eq(my).clone().appendTo($wrapper).css("width", slideWid+"%");
 	}
 }
 
 /**
  *! 이벤트 콜백
  */
+function onResize() {
+	var wid = $(this).outerWidth();
+	stageCnt = stageDefault;
+	if(wid < 576) stageCnt = (stageDefault - 4 < 1) ? 1 : stageDefault - 4;
+	else if(wid < 768) stageCnt = (stageDefault - 3 < 1) ? 1 : stageDefault - 3;
+	else if(wid < 992) stageCnt = (stageDefault - 2 < 1) ? 1 : stageDefault - 2;
+	else if(wid < 1200) stageCnt = (stageDefault - 1 < 1) ? 1 : stageDefault - 1;
 
-
-
+	slideWid = 100 / stageCnt;
+	console.log(stageCnt, slideWid);
+	init();
+}
 
 /**
  *! 이벤트 등록
  */
+$(window).resize(onResize).trigger("resize");
 
 
 
@@ -45,6 +63,3 @@ function init() {
 
 
 
-
-
-init();
