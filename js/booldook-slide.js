9ed40b545e98c3fad2ee58039ce72f74824e67df
booldook <booldook@gmail.com>
2020-12-16 18:15:59 +0900

    pager

--- js/booldook-slide.js
+++ js/booldook-slide.js
@@ -2,9 +2,9 @@
  *! 프로그램 세팅값
  */
 var container = '.booldook-slide';
-var stageViewDefault = 1;	// 스테이지에 보여질 개수 기준값(PC)
+var stageViewDefault = 4;	// 스테이지에 보여질 개수 기준값(PC)
 var moveCnt = 1; // 한번에 움직여 지는 슬라이드 개수
-var speed = 500;
+var speed = 300;
 
 
 /**
@@ -16,19 +16,21 @@ var $wrapper = $container.find('.slide-wrapper');
 var $slide = $container.find('.slide');
 var $btPrev = $container.find('.bt-prev');
 var $btNext = $container.find('.bt-next');
+var $pagerWrapper = $container.find('.pager-wrapper');
 var slideCnt = $slide.length;	// 슬라이드의 총 개수 length
 var slideLast = slideCnt - 1; // 슬라이드의 마지막 index 
 var slideWid;	// 반응형 - 슬라이드의 width
 var stageView;	// 반응형 - 스테이지에 보여질 슬라이드 개수 - 변하는 값
 var now = 0; // 기준값
 var direction = 1; // 1: 오른쪽으로 이동(Prev) / -1: 왼쪽으로 이동(Next)
-
+var target;	// Animation($wrapper)될 left값
 
 /**
  *! 사용자 함수
  */
 function init() {
 	$wrapper.empty().css("left", 0);
+	$pagerWrapper.find('.pager').removeClass('active').eq(now).addClass('active');
 	$slide.eq(now).clone().appendTo($wrapper).css("width", slideWid+"%");
 	for(var i=0, my, prev=now; i<stageView; i++) {
 		prev = my = (prev == 0) ? slideLast : prev - 1;
@@ -38,10 +40,11 @@ function init() {
 		prev = my = (prev == slideLast) ? 0 : prev + 1;
 		$slide.eq(my).clone().appendTo($wrapper).css("width", slideWid+"%");
 	}
+
 }
 
 function slideAni() {
-	$wrapper.stop().animate({"left": direction * slideWid * moveCnt + "%"}, speed, init);
+	$wrapper.stop().animate({"left": target}, speed, init);
 }
 
 /**
@@ -59,24 +62,41 @@ function onResize() {
 }
 
 function onPrev() {
-	direction = 1;
+	target = slideWid * moveCnt + "%";
 	now = now == 0 ? slideLast : now - 1;
 	slideAni();
 }
 
 function onNext() {
-	direction = -1;
+	target = -slideWid * moveCnt + "%";
 	now = now == slideLast ? 0 : now + 1;
 	slideAni();
+}
+
+function onPager() {
+	var old = now;
+	now = $(this).index();
+	if(old > now) {
+		target = '100%';
+		// 그림교체
+		slideAni();
+	}
+	if(old < now) {
+		target = '-100%';
+		// 그림교체
+		slideAni();
+	}
 }
 
 /**
  *! 이벤트 등록
  */
-$(window).resize(onResize).trigger('resize');
 $btPrev.click(onPrev);
 $btNext.click(onNext);
+for(var i=0; i<slideCnt; i++)
+	$('<i class="pager"></i>').appendTo($pagerWrapper).click(onPager);
 
+$(window).resize(onResize).trigger('resize');
 
 
 
