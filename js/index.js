39a054c0a5255a130d5cd69a1218b327c6913f8b
booldook <booldook@gmail.com>
2020-12-23 10:06:16 +0900

    수정

--- js/index.js
+++ js/index.js
@@ -114,7 +114,7 @@ function createMoNavi() {
 
 function createDepth2(idx) {
 	html  = '<div class="top-wrap">';
-	html += '	<div class="close-wrap3 bt-prev">';
+	html += '	<div class="close-wrap3 bt-prev" onclick="closeDepth(2)">';
 	html += '		<i class="fa fa-angle-left"></i>';
 	html += '	</div>';
 	html += '	<h4 class="title">'+navi[idx].name+'</h4>';
@@ -131,7 +131,9 @@ function createDepth2(idx) {
 	$(".modal-navi .depth2").addClass("active")
 }
 
-
+function closeDepth(n) {
+	$(".modal-navi .depth"+n).removeClass("active");
+}
 
 /********* 이벤트선언 **********/
 mainBanner();	// 배너세팅
@@ -262,15 +264,15 @@ function onNaviBest(r) {
 function onNaviSales(r) {
 	navi[5] = r;
 	$(".navi.navi-sales").prepend(createNavi(r));
-	for(var i=0; i<r.brands.length; i++) {
+	for(var i=0; i<r.depth2.length; i++) {
 		html  = '<div class="brand-wrap">';
-		html += '<div class="img-wrap" style="background-image: url('+r.brands[i].src+'); order: '+i%2+'">';
+		html += '<div class="img-wrap" style="background-image: url('+r.depth2[i].src+'); order: '+i%2+'">';
 		html += '</div>';
 		html += '<ul class="brand-link">';
-		html += '<li class="sub-navi bold">'+r.brands[i].company+'</li>';
-		for(var j=0; j<r.brands[i].brand.length; j++) {
+		html += '<li class="sub-navi bold">'+r.depth2[i].name+'</li>';
+		for(var j=0; j<r.depth2[i].depth3.length; j++) {
 			html += '<li class="sub-navi hover-line">';
-			html += '<a href="'+r.brands[i].brand[j].link+'">'+r.brands[i].brand[j].name+'</a>';
+			html += '<a href="'+r.depth2[i].depth3[j].link+'">'+r.depth2[i].depth3[j].name+'</a>';
 			html += '</li>';
 		}
 		html += '</ul>';
